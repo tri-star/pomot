757948f736de7f57044aa7d5fd0518526289d6f5
tristar <tristar@urban-theory.net>
2018-05-30 01:09:59 +0900

    webpackの初期化周り作業中

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,6 +1,8 @@
 'use strict'
 
 const path = require('path')
+const webpack = require('webpack')
+const {VueLoaderPlugin} = require('vue-loader')
 
 module.exports = {
     mode: 'development',
@@ -10,10 +12,51 @@ module.exports = {
         path: path.join(__dirname, '../dist')
     },
     module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['env']
+                    }
+                }
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'vue-style-loader',
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            sourceMap: true
+                        }
+                    }
 
+                ]
+
+            }
+        ]
     },
-    plugins: {
-
-    }
+    optimization: {
+        splitChunks: {
+            chunks: 'initial'
+        }
+    },
+    resolve: {
+        extensions: ['.js', '.vue'],
+        alias: {
+            'vue$': 'vue/dist/vue.esm.js',
+            '@': 'src'
+        }
+    },
+    plugins: [
+        new webpack.DefinePlugin({
+            'process.env': {
+                NODE_ENV: JSON.stringify('development')
+            }
+        }),
+        new VueLoaderPlugin
+    ]
 
 }
