146d92085c12e88141b1977d7404e71ba4a7171b
tristar <tristar@urban-theory.net>
2018-06-21 00:52:00 +0900

    外枠のレイアウトを追加

--- src/components/application.vue
+++ src/components/application.vue
@@ -1,8 +1,15 @@
 <template>
 
     <div id="application">
-        <h1>Application</h1>
-        <p>start: {{ test }}</p>
+        <header>
+            <h1>Application</h1>
+            <nav></nav>
+        </header>
+        <article class="body-container">
+            <p>start: {{ test }}</p>
+        </article>
+        <aside>Menu</aside>
+        <footer>Footer</footer>
     </div>
 
 </template>
@@ -19,3 +26,72 @@
     };
 
 </script>
+
+
+<style>
+
+@media screen and (max-width: 479px) {
+    #application {
+        display: grid;
+        grid-template-columns: 1fr;
+        grid-template-rows: 60px 60px 1fr 60px;
+    }
+
+    #application header {
+        grid-column: 1 / 2;
+        grid-row: 1 / 2;
+        background-color: #eee;
+    }
+
+    #application aside {
+        grid-column: 1 / 2;
+        grid-row: 2 / 3;
+        background-color: #ff0;
+    }
+
+    #application .body-container {
+        grid-column: 1 / 2;
+        grid-row: 3 / 4;
+        min-height: 400px;
+    }
+
+    #application footer {
+        grid-column: 1 / 2;
+        grid-row: 4 / 5;
+        background-color: #ccc;
+    }
+}
+
+@media screen and (min-width: 480px) {
+    #application {
+        display: grid;
+        grid-template-columns: 200px 1fr;
+        grid-template-rows: 200px 1fr 60px;
+    }
+
+    #application header {
+        grid-column: 1 / 3;
+        grid-row: 1 / 2;
+        background-color: #eee;
+    }
+
+    #application aside {
+        grid-column: 1 / 2;
+        grid-row: 2 / 3;
+        background-color: #ff0;
+    }
+
+    #application .body-container {
+        grid-column: 2 / 3;
+        grid-row: 2 / 3;
+        min-height: 400px;
+    }
+
+    #application footer {
+        grid-column: 1 / 3;
+        grid-row: 3 / 4;
+        background-color: #ccc;
+    }
+}
+
+</style>
