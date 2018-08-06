fe59cfab614240203c0ccfe5a2b9dba5e74c7852
tristar <tristar@urban-theory.net>
2018-08-07 01:11:46 +0900

    アプリのレイアウト周りのHTML+CSSをvueコンポーネントからBladeのレイアウトに移動

--- resources/assets/js/components/application.vue
+++ resources/assets/js/components/application.vue
@@ -1,15 +1,7 @@
 <template>
 
     <div id="application">
-        <header>
-            <h1>Pomot</h1>
-            <nav></nav>
-        </header>
-        <article class="body-container">
-            <PomotTimer/>
-        </article>
-        <aside>Menu</aside>
-        <footer>Footer</footer>
+        <PomotTimer/>
     </div>
 
 </template>
@@ -33,74 +25,5 @@
 
 <style>
 
-.clearfix:after {
-    content:" ";
-    display:block;
-    clear:both;
-}
-
-@media screen and (max-width: 479px) {
-    #application {
-        display: grid;
-        grid-template-columns: 1fr;
-        grid-template-rows: 60px 60px auto 60px;
-    }
-
-    #application header {
-        grid-column: 1 / 2;
-        grid-row: 1 / 2;
-        background-color: #eee;
-    }
-
-    #application aside {
-        grid-column: 1 / 2;
-        grid-row: 2 / 3;
-        background-color: #ff0;
-    }
-
-    #application .body-container {
-        grid-column: 1 / 2;
-        grid-row: 3 / 4;
-        min-height: 500px;
-    }
-
-    #application footer {
-        grid-column: 1 / 2;
-        grid-row: 4 / 5;
-        background-color: #ccc;
-    }
-}
-
-@media screen and (min-width: 480px) {
-    #application {
-        display: grid;
-        grid-template-columns: 200px 1fr;
-        grid-template-rows: 200px 1fr 60px;
-    }
-
-    #application header {
-        grid-column: 1 / 3;
-        grid-row: 1 / 2;
-        background-color: #eee;
-    }
-
-    #application aside {
-        grid-column: 1 / 2;
-        grid-row: 2 / 3;
-        background-color: #ff0;
-    }
-
-    #application .body-container {
-        grid-column: 2 / 3;
-        grid-row: 2 / 3;
-        min-height: 400px;
-    }
-
-    #application footer {
-        grid-column: 1 / 3;
-        grid-row: 3 / 4;
-        background-color: #ccc;
-    }
-}
 
 </style>
